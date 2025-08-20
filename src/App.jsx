443c274861762208b2a3c396deaba068da620ffe
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-08-20 19:42:16 +0200

    fix: suggerimenti

--- src/App.jsx
+++ src/App.jsx
@@ -6,43 +6,33 @@ function App() {
   const [products, setProducts] = useState([])
 
   useEffect(() => {
-    fetch(`http://localhost:3333/products`)
+    if(search.trim() === ''){
+      setProducts([])
+      return
+    }
+    fetch(`http://localhost:3333/products?search=${search}`)
       .then(res => res.json())
       .then(data => setProducts(data))
       .catch(err => console.error(err))
-  }, [])
+  }, [search])
 
-
-
-  const filteredProducts = products.filter(product => {
-    const isInBrand = product.brand.toLowerCase().includes(search.toLowerCase())
-    const isInConnect = product.connectivity.toLowerCase().includes(search.toLowerCase())
-    const isInDes = product.description.toLowerCase().includes(search.toLowerCase())
-    const isInColor = product.color.toLowerCase().includes(search.toLowerCase())
-    const isInName = product.name.toLowerCase().includes(search.toLowerCase())
-    return isInBrand || isInConnect || isInDes || isInColor || isInName
-  })
-  
 
 
   return (
     <>
+      <h1>Ricerca Dinamica</h1>
       <div className='container'>
-        <input type="text" value={search} onChange={e => setSearch(e.target.value)} placeholder='cerca' list='suggerimenti'/>
-        <datalist id='suggerimenti'>
-          {filteredProducts && filteredProducts.map((product) => {
-            return (
-              <option key={product.id} value={product.name}></option>
-            )
-          })}
-        </datalist>
-        <ul>
-          {filteredProducts && filteredProducts.map((product) => {
-            return (
-              <li key={product.id}>{product.name}</li>
-            )
-          })}
-        </ul>
+        <input type="text" value={search} onChange={e => setSearch(e.target.value)} placeholder='cerca'/>
+        {products.length > 0 && (
+          <div className='dropdown'>
+            {products.map(product=>{
+              return(
+                <p key={product.id}>{product.name}</p>
+              )
+            })}
+          </div>
+        )}
+
       </div>
     </>
   )
