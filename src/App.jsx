5340f9c5226e738f3c0d9249b123cea23e2f9d9c
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-08-20 20:44:52 +0200

    feat: implementato click con dettagli prodotto

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,24 @@ import { useState, useEffect, useCallback } from 'react'
 
 
 // funzione di debounce
-function debounce(callback, delay){
+function debounce(callback, delay) {
   let timer;
-  return (value)=>{
+  return (value) => {
     clearTimeout(timer);
-    timer= setTimeout(()=>{
+    timer = setTimeout(() => {
       callback(value);
-    },delay);
+    }, delay);
   }
 }
 
 function App() {
   const [search, setSearch] = useState('')
   const [products, setProducts] = useState([])
+  const [selectedProduct, setSelectedProduct] = useState(null)
 
   // funzione chiamata api
-  const fetchApi = (query) =>{
-    if(query.trim() === ''){
+  const fetchApi = (query) => {
+    if (query.trim() === '') {
       setProducts([])
       return
     }
@@ -26,29 +27,50 @@ function App() {
       .then(res => res.json())
       .then(data => setProducts(data))
       .catch(err => console.error(err))
-      console.log('api')
+    console.log('api')
   }
 
   // useCallback per il debounce
-  const debounceApi = useCallback(debounce(fetchApi, 500),[])
+  const debounceApi = useCallback(debounce(fetchApi, 500), [])
 
   useEffect(() => {
     debounceApi(search)
   }, [search])
 
+  function apiSelectedProduct(id) {
+    fetch(`http://localhost:3333/products/${id}`)
+      .then(res => res.json())
+      .then(data => (
+        setSelectedProduct(data),
+        setSearch(''),
+        setProducts([])
+      ))
+      .catch(err => console.error(err))
+  }
+
 
   return (
     <>
       <h1>Ricerca Dinamica</h1>
-      <div className='container'>
-        <input type="text" value={search} onChange={e => setSearch(e.target.value)} placeholder='cerca'/>
+      <input type="text" value={search} onChange={e => setSearch(e.target.value)} placeholder='cerca' />
+      <div className="container-dropdown">
         {products.length > 0 && (
           <div className='dropdown'>
-            {products.map(product=>{
-              return(
-                <p key={product.id}>{product.name}</p>
+            {products.map(product => {
+              return (
+                <p key={product.id} onClick={() => apiSelectedProduct(product.id)}>{product.name}</p>
               )
             })}
+          </div>
+        )}
+      </div>
+      <div className='container'>
+        {selectedProduct && (
+          <div className="card">
+            <h2>{selectedProduct.name}</h2>
+            <img src={selectedProduct.image} alt="" />
+            <p>{selectedProduct.description}</p>
+            <p><strong>Prezzo:</strong> {selectedProduct.price}€</p>
           </div>
         )}
       </div>
